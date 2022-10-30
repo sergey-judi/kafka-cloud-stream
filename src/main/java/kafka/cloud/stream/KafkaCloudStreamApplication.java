aca2a75f0c64fa2cf5abefa9e417805b32774f5d
sergey-judi <serega7qwerty@gmail.com>
2022-10-30 17:31:51 +0200

    Bootstrapped Gradle project

---
+++ src/main/java/kafka/cloud/stream/KafkaCloudStreamApplication.java
@@ -0,0 +1,13 @@
+package kafka.cloud.stream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaCloudStreamApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(KafkaCloudStreamApplication.class, args);
+  }
+
+}
